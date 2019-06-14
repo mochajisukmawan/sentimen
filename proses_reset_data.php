377f382a913da9476238393b9134df37079528b0
mochajisukmawan <ajie.sukmawan@gmail.com>
2019-06-14 11:15:01 +0700

    reset data

--- proses_reset_data.php
+++ proses_reset_data.php
@@ -1,14 +1,14 @@
-<?php  
+<?php
     require_once "koneksi.php";
 
     $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, 0);
 
     $sql = "
-    SET FOREIGN_KEY_CHECKS = 0; 
-    
+    SET FOREIGN_KEY_CHECKS = 0;
+
     TRUNCATE data_training;
     ALTER TABLE data_training AUTO_INCREMENT = 1;
-    
+
     TRUNCATE data_testing;
     ALTER TABLE data_testing AUTO_INCREMENT = 1;
 
@@ -18,7 +18,7 @@
     try {
         $conn->exec($sql);
         $msg = "Berhasil Reset Data Training";
-        header("location:tampil_data.php");
+        header("location:tampil_data_new.php");
     }
     catch (PDOException $e)
     {
@@ -27,4 +27,3 @@
     }
 
 ?>
-
